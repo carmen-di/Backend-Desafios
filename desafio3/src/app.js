f64e3c36cf0fe7d9f1108923e76a0868305d36b4
Carmen <cdomingueziribe@gmail.com>
2023-03-07 13:40:39 -0300

    3er desafio

---
+++ desafio3/src/app.js
@@ -0,0 +1,45 @@
+import express from "express"
+import { ProductManager } from "./ProductManager.js"
+
+const app = express()
+
+const prod = new ProductManager('./data/products.json')
+
+app.get('/products', async (req, res) => {
+    try {
+        const limit = req.query.limit
+        const productos = await prod.getProducts()
+
+        if (!limit) {
+            res.json(productos)
+        } else {
+            const productLimit = [];
+            for (let i = 0; i < limit && i < 10; i++) {
+                productLimit.push(productos[i])
+            }
+            res.json(productLimit);
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }    
+})
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const productById = await prod.getProductById(id)
+
+        if (!productById) {
+            res.json({error: 'El producto no existe'})
+        } else {
+            res.json(productById)
+        }
+    } 
+    catch (error) {
+        console.log(error);
+    }
+})
+
+
+const server = app.listen(8080)
